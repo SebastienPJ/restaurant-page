5b07c17aae11e30f927caf34128beac012c7f9b1
Sebastien <sebastienpj@protonmail.com>
2022-04-17 17:50:11 -0500

    removed all innerHTML code from homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,5 +1,4 @@
 import './styles.css'
-// import {createHomePage} from './home.js'
 import { createMenuPage } from './menu';
 import { createContactPage } from './contact'
 import Icon from './images/logo.png'
@@ -8,7 +7,7 @@ import Icon from './images/logo.png'
 
 const createHomePage = function() {
 
-    clearPage();
+    // clearPage();
   
   
     const mainDiv = document.querySelector('#content');
@@ -35,38 +34,98 @@ const createHomePage = function() {
   
   
     const callToAction = document.createElement('h2');
-    callToAction.innerHTML = 'Check out our <button class="menu-link">Menu</button>';
+    callToAction.textContent = 'Check out our ';
+
+    const actionButton = document.createElement('button');
+    actionButton.classList.add('menu-link');
+    actionButton.addEventListener('click', createMenuPage)
+    actionButton.textContent = "Menu";
+    callToAction.appendChild(actionButton);
+
+
     home.appendChild(callToAction);
   
   
     /***** FOOTER *****/
     const footer = document.createElement('footer');
     footer.classList.add('footer');
-    footer.innerHTML = `
-      <div class="address-section">
-        <h2>Find us</h2>
-        <p>3224 Street Blvd</p>
-        <p>City, State</p>
-        <p>Zip Code</p>
-        <p>000-000-0000</p>
-      </div>
-  
-  
-      <div class="operating-hours">
-        <h2> Hours</h2>
-        <p> <span>Monday - Friday:</span> 10:00 am - 11:00 pm</p>
-        <p> <span>Saturday:</span> 9:00 am - 11:30 pm</p>
-        <p> <span>Sunday</span> 9:00 am - 9:00 pm</p>
-  
-      </div>
-  
-    </div> `
+
+
+
+    const addressSection = document.createElement('div');
+    addressSection.classList.add('address-section');
+
+    const addressHeader = document.createElement('h2');
+    addressHeader.textContent = 'Find us';
+    addressSection.appendChild(addressHeader);
+
+    const addressStreet = document.createElement('p');
+    addressStreet.textContent = '3224 Street Blvd';
+    addressSection.appendChild(addressStreet);
+
+    const addressCity = document.createElement('p');
+    addressCity.textContent = 'Dallas, TX 09834';
+    addressSection.appendChild(addressCity);
+
+
+    const addressPhone = document.createElement('p');
+    addressPhone.textContent = "000-000-0000";
+    addressSection.appendChild(addressPhone);
+
+
+    footer.appendChild(addressSection);
+
+
+
+
+
+    const operatingHours = document.createElement('div');
+    operatingHours.classList.add('operating-hours');
+
+
+    const operatingHeader = document.createElement('h2');
+    operatingHeader.textContent = 'Hours';
+    operatingHours.appendChild(operatingHeader);
+    
+    const operatingTimeWeekday = document.createElement('p');
+    const MondayFridaySpan = document.createElement('span');
+    MondayFridaySpan.textContent = 'Monday - Friday:';
+    MondayFridaySpan.classList.add('days-open')
+    operatingTimeWeekday.appendChild(MondayFridaySpan);
+
+    const operatingTimeMF = document.createElement('span');
+    operatingTimeMF.textContent = ' 10:00 am - 11:00 pm';
+    operatingTimeWeekday.appendChild(operatingTimeMF);
+
+    operatingHours.appendChild(operatingTimeWeekday);
+
+
+
+
+    const operatingTimeWeekend = document.createElement('p');
+    const saturdaySpan = document.createElement('span');
+    saturdaySpan.textContent = 'Saturday - Sunday:';
+    saturdaySpan.classList.add('days-open')
+    operatingTimeWeekend.appendChild(saturdaySpan);
+
+    const operatingTimeSat = document.createElement('span');
+    operatingTimeSat.textContent = ' 11:00 am - 2:00 am';
+    operatingTimeWeekend.appendChild(operatingTimeSat);
+
+    operatingHours.appendChild(operatingTimeWeekend);
+
+
+
+
+    footer.appendChild(operatingHours);
+
+
+
+
   
     mainDiv.appendChild(footer);
   
   
-    const menuLink = document.querySelector('.menu-link');
-    menuLink.addEventListener('click', createMenuPage)
   
 };
 
@@ -129,7 +188,7 @@ const createNavBar = function() {
 
     const homeButton = document.createElement('button');
     homeButton.textContent = "Home";
-    homeButton.addEventListener("click", createHomePage);
+    // homeButton.addEventListener("click", createHomePage);
     firstLink.appendChild(homeButton);
 
 
@@ -139,7 +198,7 @@ const createNavBar = function() {
 
     const menuButton = document.createElement('button');
     menuButton.textContent = "Menu"
-    menuButton.addEventListener("click", createMenuPage);
+    // menuButton.addEventListener("click", createMenuPage);
     secondLink.appendChild(menuButton);
 
 
@@ -149,7 +208,7 @@ const createNavBar = function() {
 
     const contactButton = document.createElement('button');
     contactButton.textContent = "Contact";
-    contactButton.addEventListener("click", createContactPage);
+    // contactButton.addEventListener("click", createContactPage);
     thirdLink.appendChild(contactButton);
 
 
